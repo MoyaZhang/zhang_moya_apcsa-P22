dd12263404e388854827addb278048b0c9602d15
MoyaZhang <47190056+MoyaZhang@users.noreply.github.com>
2019-02-01 11:03:59 -0800

    Finished Unit 1 labs

---
+++ Unit01/src/StarsRunner.java
@@ -0,0 +1,25 @@
+//© A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+	   StarsAndStripes ref = new StarsAndStripes();
+
+      
+      //call the methods needed to make the patterns on the word document
+	   ref.printASmallBox();
+	   System.out.println();
+	   ref.printABigBox();
+	   
+   }
+}
